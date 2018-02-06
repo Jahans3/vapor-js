5322e2bc3ec76c7ccf4818b65e7bd6a1a21a643d
Joshua Jahans <joshua@Joshuas-MacBook-Pro.local>
2018-02-06 14:53:55 +0000

    Test arbitrary props being passed as expected...

--- test/test-app/index.js
+++ test/test-app/index.js
@@ -1,13 +1,13 @@
 // @flow
 import V from '../../src/packages/virtual-dom/index'
 
-const ArbitraryComponent = (...args) => (
-  <div>{args}</div>
+const ArbitraryComponent = ({ children }) => (
+  <div className='arbitrary-component'>{children}</div>
 )
 
 const v_tree = (
   <ArbitraryComponent>
-    <ul className='v-list'>
+    <ul className='v-list' arbitraryProp={(() => 'I should exist!')()}>
       <li className='v-list-item'>Item 1</li>
       <li className='v-list-item'>Item 2</li>
       <li className='v-list-item'>Item 3</li>
@@ -15,4 +15,4 @@ const v_tree = (
   </ArbitraryComponent>
 )
 
-console.log(v_tree)
+console.log(v_tree.children[0])
