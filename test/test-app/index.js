6dc72152a5f98bd5a78554cc0ad40286be1bac39
Joshua Jahans <joshua@Joshuas-MacBook-Pro.local>
2018-02-06 18:55:47 +0000

    Add swap button to test swap functionality

--- test/test-app/index.js
+++ test/test-app/index.js
@@ -10,7 +10,17 @@ const ArbitraryComponent = ({ children }) => (
 
 const Vtree = (
   <ul className='v-list'>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+    <li>Item 4</li>
+  </ul>
+)
 
+const Vtree2 = (
+  <ul className='v-list'>
+    <img src='https://www.watertechonline.com/wp-content/uploads/2015/11/iStock_000004135118_800x533.jpg' alt=""/>
+    <span>Welcome to Vapor!</span>
   </ul>
 )
 
@@ -22,6 +32,17 @@ console.log(Vtree)
 
 createAddButton()
 createRemoveButton()
+createSwapButton()
+
+function createSwapButton () {
+  let originalPostion = true
+  document.getElementById('update-list-swap').addEventListener('click', function () {
+    if (originalPostion) updateNode($root, Vtree2, Vtree)
+    else updateNode($root, Vtree, Vtree2)
+
+    originalPostion = !originalPostion
+  })
+}
 
 function createRemoveButton () {
   document.getElementById('update-list-remove').addEventListener('click', function () {
