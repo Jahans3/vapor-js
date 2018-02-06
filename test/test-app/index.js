a907bfcea0ba57043d902b2e6f9cb66740a3c8cd
Joshua Jahans <joshua@Joshuas-MacBook-Pro.local>
2018-02-06 14:45:03 +0000

    Test arbitrary component

--- test/test-app/index.js
+++ test/test-app/index.js
@@ -1,12 +1,18 @@
 // @flow
 import V from '../../src/packages/virtual-dom/index'
 
+const ArbitraryComponent = (...args) => (
+  <div>{args}</div>
+)
+
 const v_tree = (
-  <ul className='v-list'>
-    <li className='v-list-item'>Item 1</li>
-    <li className='v-list-item'>Item 2</li>
-    <li className='v-list-item'>Item 3</li>
-  </ul>
+  <ArbitraryComponent>
+    <ul className='v-list'>
+      <li className='v-list-item'>Item 1</li>
+      <li className='v-list-item'>Item 2</li>
+      <li className='v-list-item'>Item 3</li>
+    </ul>
+  </ArbitraryComponent>
 )
 
 console.log(v_tree)
