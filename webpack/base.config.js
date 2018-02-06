f376176b896f46b1a0db2e7e86eac7eef8621e73
Joshua Jahans <joshua@Joshuas-MacBook-Pro.local>
2018-02-06 14:36:45 +0000

    Modify build config

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = ({ name, path: filePath = 'src/', out: outPath = 'lib/' }) => ({
   entry: `./${filePath}${name}/index.js`,
   output: {
-    path: path.join(__dirname, `../${outPath}${name}`),
+    path: path.join(__dirname, `../${outPath}`),
     filename: `${name}.js`
   },
   module: {
