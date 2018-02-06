f3c06b6ee675c6b9d7a6819c5da7b3dbd6c1ea46
Joshua Jahans <joshua@Joshuas-MacBook-Pro.local>
2018-02-06 15:45:56 +0000

    Add create element layer

--- src/packages/virtual-dom/virtual-dom.js
+++ src/packages/virtual-dom/virtual-dom.js
@@ -1,9 +1,32 @@
 // @flow
 
-const V = (component, props, ...children) => ({
-  component,
-  props,
-  children
-})
+function V (component, props, ...children) {
+  return {
+    component,
+    props,
+    children
+  }
+}
+
+function createNode (node) {
+  if (typeof node === 'string') {
+    return document.createElement
+  }
+
+  const { component, children } = node
+  const element = document.createElement(component)
+
+  children.map(createNode)
+
+  return element
+}
+
+function render (app, root) {
+  root.appendChild(createNode(app))
+}
 
 export default V
+export {
+  createNode,
+  render
+}
