c0a244008c80311facd821adac052909921d52a3
Joshua Jahans <joshua@Joshuas-MacBook-Pro.local>
2018-02-06 17:16:25 +0000

    Add changed and updateNode methods, add jsdoc comments

--- src/packages/virtual-dom/virtual-dom.js
+++ src/packages/virtual-dom/virtual-dom.js
@@ -1,6 +1,14 @@
 // @flow
 import type { Component, Children, Props, Node } from './types'
 
+/**
+ * Parse a JSX node to JavaScript
+ * @param component
+ * @param props
+ * @param children
+ * @returns {{component: Component, props: V.props, children: Children[]}}
+ * @constructor
+ */
 function V (component: Component, props: Props, ...children: Children): Node {
   return {
     component,
@@ -9,6 +17,38 @@ function V (component: Component, props: Props, ...children: Children): Node {
   }
 }
 
+/**
+ * Check if a node has updated
+ * @param oldNode
+ * @param newNode
+ * @returns {boolean}
+ */
+function changed (oldNode: Node, newNode: Node): boolean {
+  return typeof oldNode !== typeof newNode || // If node1 is in any way different to node2
+    typeof oldNode === 'string' && oldNode !== newNode || // If both nodes are strings but are different
+    typeof oldNode.type !== newNode.type // If both are Vapor components but types are
+}
+
+/**
+ * Update a node
+ * @param $parent
+ * @param newNode
+ * @param oldNode
+ * @param index
+ */
+function updateNode ($parent: Object, newNode: Node, oldNode: Node, index: number = 0): void {
+  if (!oldNode) {
+    $parent.appendChild(createNode(newNode))
+  } else if (!newNode) {
+    $parent.removeChild($parent.childNodes[index])
+  }
+}
+
+/**
+ * Create a node
+ * @param node
+ * @returns {*}
+ */
 function createNode (node: Node): Object {
   if (typeof node === 'string') {
     return document.createTextNode(node)
@@ -25,13 +65,13 @@ function createNode (node: Node): Object {
     return createNode(node)
   }
 
-  const element = document.createElement(node.component)
+  const $element: Object = document.createElement(node.component)
 
   node.children.map(function (child) {
-    element.appendChild(createNode(child))
+    $element.appendChild(createNode(child))
   })
 
-  return element
+  return $element
 }
 
 function render (app: Node, root: Object): void {
