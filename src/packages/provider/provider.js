3d98b46138d66e77ba28f11a5ace843fb7266fd3
Joshua Jahans <joshua@Joshuas-MBP.ad1.tm-gnet.com>
2018-03-12 12:47:47 +0000

    add comment

--- src/packages/provider/provider.js
+++ src/packages/provider/provider.js
@@ -5,6 +5,10 @@ import type { Props } from './types'
 
 const key = '__IYA_VERSION__'
 
+/*
+  TODO
+  Convert this into a general purpose context provider - version is too iya-specific
+ */
 export function createVersionProvider (contextKey: string = key) {
   return class VersionProvider extends Component<Props> {
     static childContextTypes = {
